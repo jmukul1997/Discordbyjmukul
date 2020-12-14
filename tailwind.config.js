7d3a97f32e8c387b7262e4902ed5ba2cbaa7d02c
jmukul <dev.jmukul@users.noreply.github.com>
2020-12-15 00:30:39 +0530

    clone completed first pass

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        18: '4.5rem',
+        200: '37rem',
+      },
+      colors: {
+        'logo-hover': '#7289DA',
+        'left-sidebar' : '#202225',
+        'logo' : '#36393f',
+        'left-bg': '#2f3136',
+        'hover': '#43b581',
+        'scroll': '#2f3136',
+        'scroll-color' : '#8e9297',
+        'hovering' : '#262626',
+        'dark': '#292b2f',
+        'chat': '#3f495a',
+        'chat-color': '#dcddde',
+        'chat-color-2': '#72767d',
+        'border' : '#cbd5e0',
+        'light': '#40444b',
+        'hover-effect' : '#8e9297'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
